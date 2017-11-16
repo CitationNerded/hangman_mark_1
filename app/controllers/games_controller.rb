b2576f66f64deba38d4659c72d9a6949fb5aa297
adambeswick <adam.beswick@fluxfederation.com>
2017-11-16 16:22:49 +1300

    cleanup yolo swag

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,27 +1,5 @@
 class GamesController < ApplicationController
 
-  # def game_setup
-  #   @game = Game.new(lives: 10)
-  #   @keyword = Answer.all.sample.word
-  #   #@guess = ''
-  # end
-
-  # def create #not start
-  #   #This initializes the beginning of the game by setting the secret word and your starting lives
-  #   game_setup
-
-  #   # if @keyword.contains(@guess)
-  #   #   @correctguess += @guess
-  #   #   if @correctguess.contains(@keyword)
-  #   #     return true
-  #   #   end
-  #   # elsif @guess != @keyword
-  #   #   @game.lives -= 1
-  #   #   @incorrectguess += @guess
-  #   # else
-  #   #   return true
-  #   # end
-  # end
   def update
   end
 
