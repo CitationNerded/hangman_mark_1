a5186fdc68ccc1d49714311a792fabb24159dcc7
Adam Beswick <michaelabeswick@gmail.com>
2018-01-26 10:04:37 +1300

    comment out game logic services as it is WIP

--- app/services/game_logic.rb
+++ app/services/game_logic.rb
@@ -2,12 +2,12 @@ class GameLogic < GamesController
 #placeholder for abstraction logic -
 #not appropriate having game logic inside of model/controller
 
-def win_condition
-  if (answer_split - correct_guesses.pluck(:letter)).empty?
-    byebug
-  elsif lives <= 0
-    byebug
-  else
-    byebug
-  end
-end
+# def win_condition
+#   if (answer_split - correct_guesses.pluck(:letter)).empty?
+#     byebug
+#   elsif lives <= 0
+#     byebug
+#   else
+#     byebug
+#   end
+# end
