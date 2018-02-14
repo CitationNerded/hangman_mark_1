265e7d0fc8da4b144383bc593f4120cd2de95fb4
Adam Beswick <michaelabeswick@gmail.com>
2018-02-15 09:14:24 +1300

    tidy up some of the redundant views and remove their actions from the controller.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -29,10 +29,6 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
   end
 
-  def new
-    @game = Game.new
-  end
-
   def create
     @game = Game.create!
     redirect_to @game
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- root 'games#new'
+ root 'games#index'
  resources :games, :answers, :guesses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
